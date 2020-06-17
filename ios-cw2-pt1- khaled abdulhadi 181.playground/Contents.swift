ccf40d10b830e21ecbd36f2f014804dd544aa35a
خالد سالم عبدالهادي - ١٨١ <66526530+Xhaled5@users.noreply.github.com>
2020-06-17 20:11:58 +0300

    cw 2 ios
    khaled abdulhadi 181

---
+++ ios-cw2-pt1- khaled abdulhadi 181.playground/Contents.swift
@@ -0,0 +1,8 @@
+let family = ["Salem", "Saleh", "Khaled"]
+var evens = [2, 4, 6, 8, 10, 12]
+var grades = [95.2, 93.3, 97.1]
+var trueFalse = [true, true, true, false]
+print (family[0], family[2])
+print (evens[0], evens[5])
+print (grades[0], grades[2])
+print (trueFalse[0], trueFalse[3])
